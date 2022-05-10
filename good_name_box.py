7d8cbd6065c8a0a8c38bbb868900925efe842b72
Mathsmaniac <smithn4@middleton.school.nz>
2022-05-10 13:25:21 +1200

    Because Mr Baker told me to

---
+++ good_name_box.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+s = ttk.Style()
+root.geometry("500x400")
+mainframe = ttk.Frame(root)
+mainframe.grid(row=0, column=0)
+tk.mainloop()
